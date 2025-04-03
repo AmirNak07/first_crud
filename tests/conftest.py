13dd540330ee5d8717410a49d6b1a68da0f8b3d3
amirnak <amirnakgamer@gmail.com>
2025-04-04 00:26:32 +0300

    Add congtest.py for SQLAlchemy and test_unit_repo.py

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.core.config import settings
+from app.core.database import Base
+from app.models.user import UserProfileOrm  # noqa: F401
+
+TEST_DATABASE_URL = settings.DATABASE_URL
+pytest_plugins = ("pytest_asyncio",)
+
+
+@pytest.fixture(scope="session")
+async def async_engine():
+    engine = create_async_engine(TEST_DATABASE_URL, echo=False)
+    print(TEST_DATABASE_URL)
+    yield engine
+    await engine.dispose()
+
+
+@pytest.fixture
+async def async_test_session(async_engine: AsyncEngine):
+    async_session = async_sessionmaker(async_engine, class_=AsyncSession)
+    async with async_session() as session:
+        yield session
+        await session.commit()
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def setup_and_teardown_db(async_engine):
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
