9fc7f822a37b3f6714befc72002d237aacba2a17
amirnak <amirnakgamer@gmail.com>
2025-04-09 12:25:54 +0300

    Add comments in .env.example and config.py

--- app/core/config.py
+++ app/core/config.py
@@ -12,8 +12,7 @@ class Settings(BaseSettings):
     POSTGRES_PORT: int  # PostgreSQL port
     POSTGRES_DB: str  # PostgreSQL database name
 
-    SECRET_API_KEY: str # TODO: Add note abount this field in this file and .env file
-    # TODO: Add note in .env.example, .test.env
+    SECRET_API_KEY: str  # Secret key for API
 
     MODE: str  # Application mode (e.g. DEV, TEST, PROD)
 
